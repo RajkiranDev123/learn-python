c4e08e74f08af64a79c474c6c964ccfdf7d42ec5
RajkiranDev123 <rajtech645@gmail.com>
2026-03-10 12:25:06 +0530

    try except

---
+++ exception_handling/try_except.py
@@ -0,0 +1,21 @@
+# 3 types of error :
+
+# compile time errors : syntax like colon is missing
+
+# runtime : in the middle of execution : try/except
+
+# logical errors : 
+
+
+try:
+    num1=int(input(" no1 = "))
+    num2=int(input(" no2 = "))
+
+    try:
+        res=num1/num2
+        print(res)
+    except ZeroDivisionError:
+        print("cant divide by 0")
+
+except ValueError:
+    print("invalid input")
